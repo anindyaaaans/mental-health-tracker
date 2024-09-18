893f29253297e8de555fe5fbf84d4c26e434fb5b
Anindya Nabila Syifa <anindya031205@gmail.com>
2024-09-18 13:51:39 +0700

    Tutorial 3 Selesai

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mroce2l%m^r6pof71(3$p!wj_8=plhzcq3zirmckzak+tu#ukl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "anindya-nabila-mentalhealthtrackerr.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "anindya-nabila-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
